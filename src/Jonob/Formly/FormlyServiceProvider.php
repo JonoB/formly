cc926bc2845864affcbb50c90d483b1eb3300fef
JonoB <jon@accountsportal.com>
2013-01-13 14:09:23 +0000

    First commit

---
+++ src/Jonob/Formly/FormlyServiceProvider.php
@@ -0,0 +1,47 @@
+<?php namespace Jonob\Formly;
+
+use Illuminate\Support\ServiceProvider;
+
+class FormlyServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('jonob/formly');
+	}
+
+	/**
+	 * Register the {{full_package}} service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+        $this->app['formly'] = $this->app->share(function($app)
+        {
+            return new Formly;
+        });
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('formly');
+	}
+
+}
---
+++ src/config/config.php
@@ -0,0 +1,27 @@
+<?php
+
+return array(
+
+    'formClass' => 'form-horizontal',
+
+    'autoToken' => true,
+
+    'nameAsId' => true,
+
+    'idPrefix' => 'field_',
+
+    'requiredLabel' => '.req',
+
+    'requiredPrefix' => '',
+
+    'requiredSuffix' => ' *',
+
+    'requiredClass' => 'label-required',
+
+    'controlGroupError' => 'error',
+
+    'displayInlineErrors' => false,
+
+    'commentClass' => 'field-comment'
+
+);
